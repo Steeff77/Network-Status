a6b9c1023419be07eddb214c58af900e5a2bd035
TheIndra55 <wuppie55@gmail.com>
2017-10-13 20:22:19 +0200

    functions.php is now a class and translated comments to english

---
+++ networkstatus.class.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+class Networkstatus {
+
+	public $template;
+	
+	/**
+	* Intialize the networkstatus class
+	*
+	* @param string $template
+	* @return void
+	*/
+	function __construct($template){
+		$this->template = $template;
+	}
+	
+	/**
+	* Check for server availability
+	*
+	* @param string $domain
+	* @param int $port
+	* @return int|null Ping to the server in ms
+	*/
+	function check(string $domain, int $port)
+	{
+		$startTime = microtime(true);
+		$file = @fsockopen($domain, $port, $errno, $errstr, 1);
+		$stopTime = microtime(true);
+		$status = null;
+
+		if (false === $file) {
+			return null; // Website is offline
+		}
+
+		fclose($file);
+		return (int)(($stopTime - $startTime) * 1000);
+	}
+
+	/**
+	* Render a template
+	*
+	* @param array[] $data
+	* @return void
+	*/
+	function render(array $data)
+	{
+		require $this->template;
+	}
+}
