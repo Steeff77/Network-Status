1c3a2ade88835829398064118977501b763f84c4
TheIndra55 <wuppie55@gmail.com>
2017-10-13 23:22:18 +0200

    file cleanup

--- ajax/status.php
+++ ajax/status.php
@@ -1,6 +1,6 @@
 <?php 
 
-require_once '../networkstatus.class.php';
+require_once '../includes/networkstatus.class.php';
 require_once '../config.php';
 
 $networkstatus = new Networkstatus("");
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '/networkstatus.class.php';
+require_once __DIR__ . '/includes/networkstatus.class.php';
 require_once __DIR__ . '/config.php';
 
 $networkstatus = new Networkstatus($template);
